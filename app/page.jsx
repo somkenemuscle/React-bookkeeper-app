19f90968813c472623a27e7d1d76906cf1b772aa
“Lauren <laurenhillfreelance@gmail.com>
2023-06-10 12:00:35 +0100

    new version

--- app/page.jsx
+++ app/page.jsx
@@ -12,10 +12,10 @@ const Home = () => {
             <h1 className='header glowing-text'>Get Started With Bookie</h1>
             <h3 className='header2'>Your app for saving receipts and keeping track of daily spendings.</h3>
           </div>
-          <div className="animation-container">
+         
   
 <Image src={creditCardImage} alt="Credit Card" width={200} height={130} />
-              </div>
+             
          
           </div>
       </main>
