e634141882c351181d91ad97a584a1c859488e4a
“Lauren <laurenhillfreelance@gmail.com>
2023-06-10 09:47:38 +0100

    responsive changes v2

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import creditCardImage from '../styles/credit-card.png';
@@ -5,22 +6,22 @@ import Styles from '../styles/page.css';
 
 const Home = () => {
   return (
-    <main>
-      <div className='content-container'>
-        <div className='text-container'>
-          <h1 className='header glowing-text'>Get Started With Bookie</h1>
-          <h3 className='header2'>Your app for saving receipts and keeping track of daily spendings.</h3>
-        </div>
-        <div className="animation-container">
-          <div className="credit-card-animation spin">
-            <div className="credit-card-image-container">
-              <Image src={creditCardImage} alt="Credit Card" width={200} height={130} />
-            </div>
+      <main>
+        <div className='content-container'>
+          <div className='text-container'>
+            <h1 className='header glowing-text'>Get Started With Bookie</h1>
+            <h3 className='header2'>Your app for saving receipts and keeping track of daily spendings.</h3>
           </div>
-        </div>
-      </div>
-    </main>
-  );
-}
+          <div className="animation-container">
+  
+<Image src={creditCardImage} alt="Credit Card" width={200} height={130} />
+              </div>
+         
+          </div>
+      </main>
+      
+  )
+   
+};
 
 export default Home;
